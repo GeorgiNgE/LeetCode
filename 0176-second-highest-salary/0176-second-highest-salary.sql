05382f57f395a81bb259c2b21bde5223b9e4ba32
Georgeta Neagu <69215091+GeorgiNgE@users.noreply.github.com>
2025-05-02 12:48:33 +0300

    Time: 506 ms (41.22%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,5 +1,3 @@
-/* Write your T-SQL query statement below */
-with cte as (select e.*,
-DENSE_RANK() over(order by salary desc) as rn
-from Employee e)
+/* Write your PL/SQL query statement below */
+with cte as (select salary, dense_rank() over(order by salary desc) rn from employee group by salary)
 select max(salary) as SecondHighestSalary from cte where rn=2
